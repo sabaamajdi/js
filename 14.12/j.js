b7a22b61bdb885bc55fcb6ee67319c03107d361b
sabaamajdi0-jpg <sabaamajdi0@gmail.com>
2026-01-05 19:51:36 +0300

    5/1/2026sabaa

---
+++ 14.12/j.js
@@ -0,0 +1,38 @@
+let grade =85 ;
+
+if (grade>=90 && grade<=100){
+
+    console.log("ممتاز ");
+}
+else if (grade>=80 && grade<=89){
+
+    console.log("جيد جدا  ");
+}
+else if (grade>=70 && grade<=79){
+
+    console.log("جيد  ");
+}
+ else if (grade>=50 && grade<=69){
+
+    console.log("احسنت ");
+}
+else{console.log("حاول مجددا ");}
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
+
+let x =1 ;
+for(let i = 1; i <= 5; i++){
+x=x*i ;
+}
+console.log(x);
+
+
+ 
